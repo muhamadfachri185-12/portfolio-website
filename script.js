2c6b58aee8e6a3eccbd3a3165f4a2850f1095131
Muhammad Fachri <muhamadfachri185@gmail.com>
2026-08-07 20:53:38 +0700

    Finalisasi

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-/*-- ================= NAVBAR ================= */
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar-gradient")
-  if (window.scrollY > 50) {
-    navbar.classList.add("navbar-scrolled")
-  } else {
-    navbar.classList.remove("navbar-scrolled")
-  }
+/*-- ================= FILTER ================= */
+const filterButtons = document.querySelectorAll(".filter-btn")
+const projects = document.querySelectorAll(".project-item")
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Active Button
+    filterButtons.forEach((btn) => {
+      btn.classList.remove("btn-dark")
+      btn.classList.add("btn-outline-dark")
+    })
+    button.classList.remove("btn-dark")
+    button.classList.add("btn-outline-dark")
+
+    const filter = button.dataset.filter
+    projects.forEach((project) => {
+      const category = project.dataset.category
+
+      if (filter === "all" || category.includes(filter)) {
+        project.style.display = "block"
+      } else {
+        project.style.display = "none"
+      }
+    })
+  })
 })
